cc0a9ef272df9b15dc4b9cc4feaca038f270157f
Fabio Araujo <fabio_araujo11@hotmail.com>
2017-12-12 00:31:39 -0200

    Tela de cadastro (NÂO JOGA NO BANCO)

--- src/java/br/com/pizzadelo/web/DatabaseCreatorListener.java
+++ src/java/br/com/pizzadelo/web/DatabaseCreatorListener.java
@@ -5,11 +5,8 @@
  */
 package br.com.pizzadelo.web;
 
-
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.Statement;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -19,103 +16,105 @@ import javax.servlet.ServletContextListener;
  * @author vlf
  */
 public class DatabaseCreatorListener implements ServletContextListener {
-    
-    private void createUsuarioTable(Statement s){
-        try{
-            s.execute(  "CREATE TABLE usuario("
-            		+ "cpf_usuario INTEGER PRIMARY KEY"
-           		+ ", nm_email_usuario VARCHAR(255) NOT NULL"
-                        + ", nm_usuario VARCHAR(255) NOT NULL"
-          		+ ", nm_login_usuario VARCHAR(30) NOT NULL" 
-           		+ ", password_user varchar(255) NOT NULL"
-          		+ ", nm_tipo_usuario CHAR(1) NOT NULL"         
-                   	+ ")");
-            
-          
 
-                 
-            System.out.println("Criada tabela USUARIO.");	
-           
-      
-        }catch(Exception ex2){
-            System.out.println("Erro ao criar a tabela USUARIO: "+ex2.getMessage());
+    private void createUsuarioTable(Statement s) {
+        try {
+            s.execute("CREATE TABLE usuario("
+                    + "cpf_usuario DOUBLE PRIMARY KEY"
+                    + ", nm_email_usuario VARCHAR(255) NOT NULL"
+                    + ", nm_usuario VARCHAR(255) NOT NULL"
+                    + ", password_user varchar(255) NOT NULL"
+                    + ", nm_tipo_usuario CHAR(1) NOT NULL"
+                    + ", ic_sexo_m_f CHAR(1) NOT NULL"
+                    + ")");
+            s.execute("INSERT INTO APP.USUARIO VALUES("
+                    + "123456789"
+                    + ", 'admin@root.com.br'"
+                    + ", 'Administrador do Sistema'"
+                    + ", 'root'"
+                    + ", 'A'"
+                    + ", 'M'"
+                    + ")");
+
+            System.out.println("Criada tabela USUARIO.");
+
+        } catch (Exception ex2) {
+            System.out.println("Erro ao criar a tabela USUARIO: " + ex2.getMessage());
         }
     }
 
- 
-    private void createItemTable(Statement s){
-        try{
+    private void createItemTable(Statement s) {
+        try {
             s.execute("CREATE TABLE item ("
-          		+ " cd_item INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1)"
-	   		+ ", vl_item numeric(10,2) NOT NULL "
-	   		+ ", ds_item VARCHAR(255) NOT NULL"	 	         
-           		+ ", nm_item VARCHAR(30) NOT NULL"
-                        + ", tipo_item VARCHAR(30) NOT NULL"
-                   	+ ")");
-            
-            s.execute ("INSERT INTO APP.ITEM (VL_ITEM, DS_ITEM, NM_ITEM, TIPO_ITEM) \n" +
-"	VALUES (30.00, 'Pizza de', 'Portuguesa', 'PIZZA')");
-            s.execute("INSERT INTO APP.ITEM (VL_ITEM, DS_ITEM, NM_ITEM, TIPO_ITEM) \n" +
-"	VALUES (30.00, 'Pizza de', 'Mussarela', 'PIZZA')");
-            s.execute("INSERT INTO APP.ITEM (VL_ITEM, DS_ITEM, NM_ITEM, TIPO_ITEM) \n" +
-"	VALUES (30.00, 'Pizza de', 'Calabresa', 'PIZZA')");
-            s.execute("INSERT INTO APP.ITEM (VL_ITEM, DS_ITEM, NM_ITEM, TIPO_ITEM) \n" +
-"	VALUES (30.00, 'Pizza de', 'Frango com Catupiry', 'PIZZA')");
-            s.execute("INSERT INTO APP.ITEM (VL_ITEM, DS_ITEM, NM_ITEM, TIPO_ITEM) \n" +
-"	VALUES (30.00, 'Pizza de', 'Quatro Queijos', 'PIZZA')");
-            s.execute("INSERT INTO APP.ITEM (VL_ITEM, DS_ITEM, NM_ITEM, TIPO_ITEM) \n" +
-"	VALUES (30.00, 'Pizza de', 'Tomate', 'PIZZA')");
-            
-           s.execute("INSERT INTO APP.ITEM (VL_ITEM, DS_ITEM, NM_ITEM, TIPO_ITEM) \n" +
-"	VALUES (5.00, '-', 'Coca-Cola', 'BEBIDA')");
-           s.execute("INSERT INTO APP.ITEM (VL_ITEM, DS_ITEM, NM_ITEM, TIPO_ITEM) \n" +
-"	VALUES (5.00, '-', 'Agua', 'BEBIDA')");
-           s.execute("INSERT INTO APP.ITEM (VL_ITEM, DS_ITEM, NM_ITEM, TIPO_ITEM) \n" +
-"	VALUES (5.00, '-', 'Guarana', 'BEBIDA')");
-           s.execute("INSERT INTO APP.ITEM (VL_ITEM, DS_ITEM, NM_ITEM, TIPO_ITEM) \n" +
-"	VALUES (5.00, '-', 'Fanta Laranja', 'BEBIDA')");
-           s.execute("INSERT INTO APP.ITEM (VL_ITEM, DS_ITEM, NM_ITEM, TIPO_ITEM) \n" +
-"	VALUES (5.00, '-', 'Sprite', 'BEBIDA')");
-            System.out.println("Criada tabela item.");            
-        }catch(Exception ex2){
-            System.out.println("Erro ao criar a tabela item: "+ex2.getMessage());
+                    + " cd_item INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1)"
+                    + ", vl_item numeric(10,2) NOT NULL "
+                    + ", ds_item VARCHAR(255) NOT NULL"
+                    + ", nm_item VARCHAR(30) NOT NULL"
+                    + ", tipo_item VARCHAR(30) NOT NULL"
+                    + ")");
+
+            s.execute("INSERT INTO APP.ITEM (VL_ITEM, DS_ITEM, NM_ITEM, TIPO_ITEM) \n"
+                    + "	VALUES (30.00, 'Pizza de', 'Portuguesa', 'PIZZA')");
+            s.execute("INSERT INTO APP.ITEM (VL_ITEM, DS_ITEM, NM_ITEM, TIPO_ITEM) \n"
+                    + "	VALUES (30.00, 'Pizza de', 'Mussarela', 'PIZZA')");
+            s.execute("INSERT INTO APP.ITEM (VL_ITEM, DS_ITEM, NM_ITEM, TIPO_ITEM) \n"
+                    + "	VALUES (30.00, 'Pizza de', 'Calabresa', 'PIZZA')");
+            s.execute("INSERT INTO APP.ITEM (VL_ITEM, DS_ITEM, NM_ITEM, TIPO_ITEM) \n"
+                    + "	VALUES (30.00, 'Pizza de', 'Frango com Catupiry', 'PIZZA')");
+            s.execute("INSERT INTO APP.ITEM (VL_ITEM, DS_ITEM, NM_ITEM, TIPO_ITEM) \n"
+                    + "	VALUES (30.00, 'Pizza de', 'Quatro Queijos', 'PIZZA')");
+            s.execute("INSERT INTO APP.ITEM (VL_ITEM, DS_ITEM, NM_ITEM, TIPO_ITEM) \n"
+                    + "	VALUES (30.00, 'Pizza de', 'Tomate', 'PIZZA')");
+
+            s.execute("INSERT INTO APP.ITEM (VL_ITEM, DS_ITEM, NM_ITEM, TIPO_ITEM) \n"
+                    + "	VALUES (5.00, '-', 'Coca-Cola', 'BEBIDA')");
+            s.execute("INSERT INTO APP.ITEM (VL_ITEM, DS_ITEM, NM_ITEM, TIPO_ITEM) \n"
+                    + "	VALUES (5.00, '-', 'Agua', 'BEBIDA')");
+            s.execute("INSERT INTO APP.ITEM (VL_ITEM, DS_ITEM, NM_ITEM, TIPO_ITEM) \n"
+                    + "	VALUES (5.00, '-', 'Guarana', 'BEBIDA')");
+            s.execute("INSERT INTO APP.ITEM (VL_ITEM, DS_ITEM, NM_ITEM, TIPO_ITEM) \n"
+                    + "	VALUES (5.00, '-', 'Fanta Laranja', 'BEBIDA')");
+            s.execute("INSERT INTO APP.ITEM (VL_ITEM, DS_ITEM, NM_ITEM, TIPO_ITEM) \n"
+                    + "	VALUES (5.00, '-', 'Sprite', 'BEBIDA')");
+            System.out.println("Criada tabela item.");
+        } catch (Exception ex2) {
+            System.out.println("Erro ao criar a tabela item: " + ex2.getMessage());
         }
-    }    
-    
-    private void createPedidoTable(Statement s){
-        try{
+    }
+
+    private void createPedidoTable(Statement s) {
+        try {
             s.execute("CREATE TABLE pedido("
-           		+ "cd_pedido INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1)"
-           		+ ", cpf_user INTEGER"           
-           		+ ", vl_total_pedido numeric(10,2)"           
-           		+ ", dt_pedido DATE"
-           		+ ", nm_estado_pedido VARCHAR(30)"
-           		+ ", CONSTRAINT pedido_fk1 FOREIGN KEY (cpf_user) REFERENCES usuario(cpf_usuario)"
-                   	+ ")");
+                    + "cd_pedido INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1)"
+                    + ", cpf_user INTEGER"
+                    + ", vl_total_pedido numeric(10,2)"
+                    + ", dt_pedido DATE"
+                    + ", nm_estado_pedido VARCHAR(30)"
+                    + ", CONSTRAINT pedido_fk1 FOREIGN KEY (cpf_user) REFERENCES usuario(cpf_usuario)"
+                    + ")");
             System.out.println("Criada tabela pedido.");
-        }catch(Exception ex2){
-            System.out.println("Erro ao criar a tabela pedido: "+ex2.getMessage());
+        } catch (Exception ex2) {
+            System.out.println("Erro ao criar a tabela pedido: " + ex2.getMessage());
         }
     }
 
-    private void createPedidoItemTable(Statement s){
-        try{
-            s.execute(  "CREATE TABLE pedido_item("
-          		+ "cd_pedido INTEGER"
-	  		+ ", cd_item INTEGER"                        
-           		+ ", CONSTRAINT pedidoitem_fk1 FOREIGN KEY (cd_pedido) REFERENCES PEDIDO(cd_pedido)"
-           		+ ", CONSTRAINT pedidoitem_fk2 FOREIGN KEY (cd_item) REFERENCES ITEM(cd_item)"                        
-                   	+ ")");
+    private void createPedidoItemTable(Statement s) {
+        try {
+            s.execute("CREATE TABLE pedido_item("
+                    + "cd_pedido INTEGER"
+                    + ", cd_item INTEGER"
+                    + ", CONSTRAINT pedidoitem_fk1 FOREIGN KEY (cd_pedido) REFERENCES PEDIDO(cd_pedido)"
+                    + ", CONSTRAINT pedidoitem_fk2 FOREIGN KEY (cd_item) REFERENCES ITEM(cd_item)"
+                    + ")");
             System.out.println("Criada tabela pedido_item.");
-        }catch(Exception ex2){
-            System.out.println("Erro ao criar a tabela pedido_item: "+ex2.getMessage());
+        } catch (Exception ex2) {
+            System.out.println("Erro ao criar a tabela pedido_item: " + ex2.getMessage());
         }
     }
 
-    
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        try{
+        try {
             Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
             String url = "jdbc:derby:c:/derby/ProjetoPizza;create=true";
             Connection c = DriverManager.getConnection(url);
@@ -124,18 +123,18 @@ public class DatabaseCreatorListener implements ServletContextListener {
             createUsuarioTable(s);
             createItemTable(s);
             createPedidoTable(s);
-	    createPedidoItemTable(s);            
-    
+            createPedidoItemTable(s);
+
             s.close();
             c.close();
-            DriverManager.getConnection
-            ("jdbc:derby:c:/derby/ProjetoPizza;shutdown=true");
-        }catch(Exception ex){
-            System.out.println("Erro: "+ex.getMessage());
+            DriverManager.getConnection("jdbc:derby:c:/derby/ProjetoPizza;shutdown=true");
+        } catch (Exception ex) {
+            System.out.println("Erro: " + ex.getMessage());
         }
     }
+
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-        
+
     }
 }
